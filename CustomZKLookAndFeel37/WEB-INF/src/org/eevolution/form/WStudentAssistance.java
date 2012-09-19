6e2cb285281d1cf701efee723d2d871b8976bba5
JosVM <eevolution@172.16.40.211>
2012-09-19 16:55:12 -0500

    Update WStudentAssistance

--- CustomZKLookAndFeel37/WEB-INF/src/org/eevolution/form/WStudentAssistance.java
+++ CustomZKLookAndFeel37/WEB-INF/src/org/eevolution/form/WStudentAssistance.java
@@ -28,12 +28,12 @@ import org.adempiere.webui.panel.CustomForm;
 import org.adempiere.webui.panel.IFormController;
 import org.adempiere.webui.panel.StatusBarPanel;
 import org.adempiere.webui.window.FDialog;
+import org.compiere.minigrid.IDColumn;
 import org.compiere.model.MRefList;
 import org.compiere.util.DB;
 import org.compiere.util.Env;
 import org.compiere.util.Msg;
 import org.fcaq.model.X_CA_CourseDef;
-import org.fcaq.model.X_CA_Parcial;
 import org.fcaq.model.X_CA_SubjectMatter;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.EventListener;
@@ -64,6 +64,8 @@ implements IFormController, EventListener, WTableModelListener, ValueChangeListe
 	private WDateEditor fDate = null;
 	private WTableDirEditor fCourse = null;
 	
+	private boolean setting = false;
+	
 	
 	public WStudentAssistance()
 	{
@@ -73,7 +75,7 @@ implements IFormController, EventListener, WTableModelListener, ValueChangeListe
 			
 			dynInit();
 			zkInit();
-			loadCandidateTable();
+			loadAssistanceTable();
 			southPanel.appendChild(new Separator());
 			southPanel.appendChild(statusBar);
 		}
@@ -174,8 +176,8 @@ implements IFormController, EventListener, WTableModelListener, ValueChangeListe
 		}
 		
 		
-		private void loadCandidateTable() {
-			Vector<Vector<Object>> data = getAssistanceData(fCourse.getValue(), fSubject.getValue(), fDate.getValue());
+		private void loadAssistanceTable() {
+			Vector<Vector<Object>> data = getEmptyData();
 			Vector<String> columnNames = getColumnNames();
 			
 			studentTable.clear();
@@ -185,13 +187,17 @@ implements IFormController, EventListener, WTableModelListener, ValueChangeListe
 			modelP.addTableModelListener(this);
 			studentTable.setData(modelP, columnNames);
 			
-			studentTable.setColumnClass(0, String.class, true);			//  0-Name
-			studentTable.setColumnClass(1, Checkbox.class, false);		//  1-Assistance
-			studentTable.setColumnClass(2, Checkbox.class, false);		//  2-Absence
-			studentTable.setColumnClass(3, Checkbox.class, false); 		//  3-Dealy
-			studentTable.setColumnClass(4, Combobox.class, false);		//  4-Motive
-			studentTable.setColumnClass(5, Combobox.class, false);		//  5-Comment
-			studentTable.setColumnClass(6, String.class, false);		// 6-Observations
+			studentTable.setColumnClass(0, IDColumn.class, true);		//  0-ID
+			studentTable.setColumnClass(1, String.class, true);			//  1-Name
+			studentTable.setColumnClass(2, String.class, true);			//  2-Course
+			studentTable.setColumnClass(3, Checkbox.class, false);		//  3-Assistance
+			studentTable.setColumnClass(4, Checkbox.class, false);		//  4-Absence
+			studentTable.setColumnClass(5, Checkbox.class, false); 		//  5-Delay
+			studentTable.setColumnClass(6, Combobox.class, false);		//  6-Motive
+			studentTable.setColumnClass(7, Combobox.class, false);		//  7-Comment
+			studentTable.setColumnClass(8, String.class, false);		//  8-Observations
+			
+			studentTable.addActionListener(this);
 			
 		}
 	
@@ -219,13 +225,38 @@ implements IFormController, EventListener, WTableModelListener, ValueChangeListe
 	
 	@Override
 	public void tableChanged(WTableModelEvent event) {
-		// TODO Auto-generated method stub
-		
+		//Checkbox & Observations
+		if (!setting)
+			setAssistenceRow(event.getLastRow(), event.getColumn());	
 	}
 	
 	@Override
 	public void onEvent(Event event) throws Exception {
-		// TODO Auto-generated method stub
+		//cambia combobox
+		if(event.getTarget() instanceof Combobox)
+		{
+			int rowNo = -1;
+			int colNo = -1;
+			Combobox currentCombo = (Combobox)event.getTarget();
+			
+			// find current row
+			for(int x=0; x <= studentTable.getRowCount()-1;x++)
+			{
+				Combobox motiveCombo = (Combobox)studentTable.getValueAt(x,6);
+				Combobox commentCombo = (Combobox)studentTable.getValueAt(x,7);
+				
+				if(motiveCombo == currentCombo) {
+					rowNo = x;
+					colNo = 6;
+				}
+				if(commentCombo == currentCombo) {
+					rowNo = x;
+					colNo = 7;
+				}
+			}
+			if (!setting)
+				setAssistenceRow(rowNo, colNo);
+		}
 		
 	}
 	
@@ -247,6 +278,7 @@ implements IFormController, EventListener, WTableModelListener, ValueChangeListe
 	@Override
 	public Object getComments() {
 		Combobox commentBox = new Combobox();
+		commentBox.setWidth("100%");
 		
 		String name = null;
 		for (MRefList comment : comments) {
@@ -262,14 +294,17 @@ implements IFormController, EventListener, WTableModelListener, ValueChangeListe
 			else
 				commentBox.appendItem(name, comment.getValue());
 		}
+		commentBox.addEventListener("onChange", this);
 		return commentBox;
 	}
 	
 	@Override
 	public Object getMotive() {
 		Combobox motiveBox = new Combobox();
+		motiveBox.setWidth("100%");
 		motiveBox.appendItem(Msg.translate(m_ctx, "Justified"), "J");
 		motiveBox.appendItem(Msg.translate(m_ctx, "Unjustified"), "U");
+		motiveBox.addEventListener("onChange", this);
 		return motiveBox;
 	}
 	
@@ -281,14 +316,175 @@ implements IFormController, EventListener, WTableModelListener, ValueChangeListe
 		
 		currentCourse  = new X_CA_CourseDef(m_ctx, (Integer)fCourse.getValue(), null);
 		currentSubject = new X_CA_SubjectMatter(m_ctx, (Integer)fSubject.getValue(), null);
-		
 
+		Vector<Vector<Object>> data = getAssistanceData();
 		
+		studentTable.clear();
+		studentTable.getModel().removeTableModelListener(this);
+		
+		ListModelTable modelP = new ListModelTable(data);
+		modelP.addTableModelListener(this);
+		studentTable.setData(modelP, getColumnNames());
+		
+		studentTable.setColumnClass(0, IDColumn.class, true);		//  0-ID
+		studentTable.setColumnClass(1, String.class, true);			//  1-Name
+		studentTable.setColumnClass(2, String.class, true);			//  2-Course
+		studentTable.setColumnClass(3, Checkbox.class, false);		//  3-Assistance
+		studentTable.setColumnClass(4, Checkbox.class, false);		//  4-Absence
+		studentTable.setColumnClass(5, Checkbox.class, false); 		//  5-Delay
+		studentTable.setColumnClass(6, Combobox.class, false);		//  6-Motive
+		studentTable.setColumnClass(7, Combobox.class, false);		//  7-Comment
+		studentTable.setColumnClass(8, String.class, false);		//  8-Observations
+		
+		studentTable.addActionListener(this);
+	
+		
+		refreshAssistance();
+		
+	}
+	
+	
+	public void setAssistanceData(int bPartner_ID, boolean assistance, boolean absence, 
+			boolean delay, String motive, String comment, String observation) {
+		setting = true;
+		for (int i=0; i <= studentTable.getRowCount()-1; i++){
+			
+			int studentTable_ID = (Integer) studentTable.getValueAt(i, 0);
+			
+			if (studentTable_ID > 0 && studentTable_ID == bPartner_ID){
+				
+				studentTable.setValueAt(assistance, i, 3);
+				studentTable.setValueAt(absence, i, 4);
+				studentTable.setValueAt(delay, i, 5);
+				studentTable.setValueAt(selectItemMotive(motive), i, 6);
+				studentTable.setValueAt(selectItemComment(comment), i, 7);
+				
+				if (observation == null)
+					studentTable.setValueAt("", i, 8);
+				else
+					studentTable.setValueAt(observation, i, 8);
+				
+				break;
+			}
+		}
+		setting = false;
+	}
+	
+	public Combobox selectItemComment(String value){
+		
+		Combobox commentBox = (Combobox)getComments();
+				
+		for (int index = 0; index <= commentBox.getItemCount()-1; index++) {
+			if (value != null && value.equals((String)commentBox.getItemAtIndex(index).getValue())) {
+				commentBox.setSelectedItem(commentBox.getItemAtIndex(index));
+				break;
+			}
+		}
+		return commentBox;
+	}
+	
+	public Combobox selectItemMotive(String value) {
+		
+		Combobox motiveBox = (Combobox)getMotive();
+		
+		for (int index = 0; index <= motiveBox.getItemCount()-1; index++) {
+			if (value != null && value.equals((String)motiveBox.getItemAtIndex(index).getValue())) {
+				motiveBox.setSelectedItem(motiveBox.getItemAtIndex(index));
+				break;
+			}
+		}
+		return motiveBox;
 	}
 	
 	private void clean()
 	{
 		
+	}
+	
+	public void setAssistenceRow(int row, int column){
+		setting = true;
+		
+		int studentTable_ID = (Integer) studentTable.getValueAt(row, 0);
+		boolean assistance = (Boolean) studentTable.getValueAt(row, 3);
+		boolean absence = (Boolean) studentTable.getValueAt(row, 4);
+		boolean delay = (Boolean) studentTable.getValueAt(row, 5);
+		Combobox motiveBox = (Combobox)studentTable.getValueAt(row, 6);
+		Combobox commentBox = (Combobox)studentTable.getValueAt(row, 7);
+		String observation = (String)studentTable.getValueAt(row, 8);
+		
+		if (column == 3 && !assistance) {
+			studentTable.setValueAt(true, row, 3);
+			setting = false;
+			return ;
+		} else if (column == 4 && !absence) {
+			studentTable.setValueAt(true, row, 4);
+			setting = false;
+			return ;
+		} else if (column == 5 && !delay) {
+			studentTable.setValueAt(true, row, 5);
+			setting = false;
+			return ;
+		} else if (column == 3 && assistance) {
+			studentTable.setValueAt(false, row, 4);
+			studentTable.setValueAt(false, row, 5);
+			studentTable.setValueAt((Combobox)getMotive(), row, 6);
+			studentTable.setValueAt((Combobox)getComments(), row, 7);
+			studentTable.setValueAt("", row, 8);
+			absence = false;
+			delay = false;
+		} else if (column == 4 && absence) {
+			studentTable.setValueAt(false, row, 3);
+			studentTable.setValueAt(false, row, 5);
+			assistance = false;
+			delay = false;
+			
+		} else if (column == 5 && delay) {
+			studentTable.setValueAt(false, row, 3);
+			studentTable.setValueAt(false, row, 4);
+			assistance = false;
+			absence = false;
+		} else if (column == 6 && (!absence && !delay)) {
+			studentTable.setValueAt(false, row, 3);
+			studentTable.setValueAt(false, row, 4);
+			studentTable.setValueAt(true, row, 5);
+			assistance = false;
+			absence = false;
+			delay = true;
+		} else if (column == 7 && (!absence && !delay)) {
+			studentTable.setValueAt(false, row, 3);
+			studentTable.setValueAt(false, row, 4);
+			studentTable.setValueAt(true, row, 5);
+			assistance = false;
+			absence = false;
+			delay = true;
+		} else if (column == 8 && (!absence && !delay)) {
+			studentTable.setValueAt(false, row, 3);
+			studentTable.setValueAt(false, row, 4);
+			studentTable.setValueAt(true, row, 5);
+			assistance = false;
+			absence = false;
+			delay = true;
+		} else 
+			;
+		
+		
+		int indexMotive = motiveBox.getSelectedIndex();
+		String motive = "U";
+		if (indexMotive >= 0) {
+			motive = (String)motiveBox.getSelectedItem().getValue();
+		} else {
+			motiveBox.setSelectedIndex(1);
+			studentTable.setValueAt(motiveBox, row, 6);
+		}
+		int indexComment = commentBox.getSelectedIndex();
+		String comment = null;
+		if (indexComment >= 0) {
+			comment = (String)commentBox.getSelectedItem().getValue();
+		}
+		
+		setAssistanceLine(studentTable_ID, assistance, absence, delay, motive, comment, observation);
+		
+		setting = false;
 	}
 	
 }
